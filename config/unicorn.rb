567a5767b4b17f4b550d0ea12506c750f547368c
Rami Grossman <ramigg@gmail.com>
2011-12-04 12:33:34 +0200

    setup fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@
 
 # 16 workers and 1 master
 #worker_processes (rails_env == 'production' ? 16 : 4)
-worker_processes (Rails.env.production? ? 16 : 4)
+worker_processes (::Rails.env.production? ? 16 : 4)
 
 # Load rails+github.git into the master before forking workers
 # for super-fast worker spawn times
@@ -39,7 +39,7 @@ before_fork do |server, worker|
   #
   # Using this method we get 0 downtime deploys.
 
-  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = ::Rails.root + '/tmp/pids/unicorn.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
@@ -79,7 +79,7 @@ after_fork do |server, worker|
     end
   rescue => e
     #if RAILS_ENV == 'development'
-    if Rails.env.development?
+    if ::Rails.env.development?
       STDERR.puts "couldn't change user, oh well"
     else
       raise e
