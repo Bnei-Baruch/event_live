4895fc3307bebf3d539d5b630cba0f6350837b21
Rami Grossman <ramigg@gmail.com>
2011-12-04 13:34:39 +0200

    widget admin tweaks

--- app/admin/widgets.rb
+++ app/admin/widgets.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register Widget do
+  index do
+    column :id
+    column :title
+    column :content
+    column :updated_at
+    column "Actions" do |widget|
+      link_to("View", admin_widget_path(widget)) +
+      "&nbsp;&nbsp;&nbsp;".html_safe +
+      link_to("Edit", edit_admin_widget_path(widget))
+    end
+  end
+
+
   form do |f|
     f.inputs "Widget Details" do
+      f.input :title
       f.input :content, :input_html => { :class => "ckeditor" }
     end
     f.buttons
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,3 +1,3 @@
 class Widget < ActiveRecord::Base
-
+  validates :system_name, :uniqueness => true, :presence => true
 end
